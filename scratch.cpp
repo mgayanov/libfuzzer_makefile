9135dce77fa239ca08a1b248c2b0b47a38d93caf
Marat <m.gayanov@gmail.com>
2020-08-01 15:18:30 +0300

    initial

---
+++ scratch.cpp
@@ -0,0 +1,11 @@
+#include "src/libfuzzer/libfuzzer_macro.h"
+#include "scratch.pb.h"
+
+#include <string>
+#include <iostream>
+#include <stdio.h>
+
+DEFINE_PROTO_FUZZER(const fuzz_package::FuzzData& fuzz_data){
+
+
+}
