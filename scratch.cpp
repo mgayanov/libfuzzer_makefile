7547f5ecbefa295577659c846872adf8490683a6
Marat <m.gayanov@gmail.com>
2020-08-05 14:55:44 +0300

    more specific

--- scratch.cpp
+++ scratch.cpp
@@ -7,5 +7,12 @@
 
 DEFINE_PROTO_FUZZER(const fuzz_package::FuzzData& fuzz_data){
 
+    if(fuzz_data.a() > 100 && 
+       fuzz_data.b() < 30  &&
+       fuzz_data.c() == 10 &&
+       fuzz_data.d() == "abc" ){
+           std::cout << "hello there!" << std::endl;
+           abort();
+       }
 
 }
